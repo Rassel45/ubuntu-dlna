ab9116d81bc9f3cbd4fabb1b5e7f4f11860a9447
Rassel45 <99266862+Rassel45@users.noreply.github.com>
2023-05-12 17:30:47 +0000

    done

--- PR1-UF4/DLNA-UBUNTU/run.sh
+++ PR1-UF4/DLNA-UBUNTU/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#
 docker build -t docker-dlna .
-docker network create --subnet=2.18.0.0/16 mynet123
-docker run -d --net mynet123 --ip 2.18.0.22 --name dlna-server -p 1900:1900/udp -p 8200:8200/tcp -v $(pwd)/media:/home/user -v /dev/shm:/dev/shm docker-dlna
+#
+docker run -d --network host --name dlna-server -p 1900:1900/udp -p 8200:8200/tcp -v $(pwd)/media:/home/user -v /dev/shm:/dev/shm docker-dlna
